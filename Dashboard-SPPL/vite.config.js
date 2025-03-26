4f027bb20a31978d2bd2dc07d043c238d60ccc6d
priyanshu17291 <priyanshu.mcp@gmail.com>
2025-03-26 17:49:16 +0530

    deployed two

--- Dashboard-SPPL/vite.config.js
+++ Dashboard-SPPL/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+
+// Determine if running in GitHub Pages (build mode)
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === "production" ? "/SPPL/Dashboard-SPPL/" : "/", // ✅ Works on localhost & GitHub Pages
+}));
